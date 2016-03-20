1cf4237be2b7d3fda1278c61de1d497a5d3058ce
Karin Fernandez <karinfdez@yahoo.com>
2016-03-20 09:02:36 -0400

    Fixed close button on modal

--- Yolyanko_project/Yolyanko_portfolio/portfolio_project/js/page_effects.js
+++ Yolyanko_project/Yolyanko_portfolio/portfolio_project/js/page_effects.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	// Show image on modal
     $('.materialboxed').materialbox();
+    
+    // To close modal in index page
+    $('.modal-trigger').leanModal({
+		dismissible: false
+	});
   });
+
+
